e1f044fbe088695fe5e8126a4da9844c0ae55959
BolyWi@mi <zzls-cool@foxmail.com>
2025-05-11 13:33:21 +0800

    add io_demo.cpp

--- STL/io_demo.cpp
+++ STL/io_demo.cpp
@@ -12,9 +12,41 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
 
-void os_demo()
+using namespace std;
+void file_op(string file_name, string content)
+{
+    fstream file;
+    file.open(file_name, fstream::app);
+    file.write(content.c_str(), content.length());
+	cout << content;
+	file.close();
+}
+
+void* update_time(void* task)
+{
+    char buf[32] = {0};
+    char file_name[64] = { 0 };
+    time_t seed = 0, old_seed = -1;
+    while(1)
+    {
+        time(&seed);
+        if(seed!= old_seed)
+        {
+            old_seed = seed;
+            struct tm* now_time = localtime(&seed);
+            strftime(file_name, sizeof(file_name), "log_20%y-%m-%d.txt", now_time);
+            strftime(buf, sizeof(buf), "20%y-%m-%d_%H:%M:%S\n", now_time);
+            file_op(string(file_name), string(buf));
+        }
+        usleep(1000*100);
+    }
+}
+
+void io_op()
 {
     cin.rdstate();
     cin.clear();
@@ -30,7 +62,12 @@ void os_demo()
 
 int main()
 {
-    string *str =
-    printf("%s", str[1].c_str());
+    pthread_t colck_task;
+    pthread_create(&colck_task, NULL, update_time, NULL);
+	pthread_create(&colck_task, NULL, update_time, NULL);
+	while(1)
+    {
+        usleep(1000*10);
+    }
     return 0;
 }
